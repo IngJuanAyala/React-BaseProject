fa0951768b7e52b9ff2121ce683c07a304f802e7
JuanAyal <juanayalasalazar1@gmail.com>
2020-03-03 15:16:14 -0500

    se hace instalación de styled component y se hace un pequeño ejemplo con botones

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 
-const HelloWorld = () => <h1>Holita Mundo!</h1>;
+const Button = styled.button`
+  background: ${props => props.theme.base};
+  color: white;
+  font-size: 1rem;
+  padding: 0.75rem 2rem;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  border: none;
+  border-radius: 4px;
+  margin: 5rem;
+  translation: all 0.1s;
 
+  &:hover {
+    transform: translateY(1px);
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  }
+`;
+
+const ButtonDanger = styled(Button)`
+  background: ${props => props.theme.danger};
+`;
+
+const ButtonGradient = styled(Button)`
+  background: ${props => props.theme.gradient};
+`;
+
+const HelloWorld = () => (
+  <>
+    <h1>Holita Mundo!</h1>
+    <Button>Basic Button</Button>
+    <ButtonDanger>Danger Button</ButtonDanger>
+    <ButtonGradient>Gradient Button</ButtonGradient>
+  </>
+);
 export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HelloWorld from "./components/HelloWorld";
+import { ThemeProvider } from "styled-components";
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+const theme = {
+  base: "#a04ed9",
+  danger: "tomato",
+  gradient: `linear-gradient(225deg, #00DBDE 0%, #FC00FF 100%);`
+};
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <HelloWorld />
+  </ThemeProvider>,
+  document.getElementById("app")
+);
